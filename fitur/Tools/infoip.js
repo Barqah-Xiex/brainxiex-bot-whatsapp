377269f9493f477c361fe619975636eca51f27ee
Barqah-Xiex <barqahgaming@gmail.com>
2025-12-31 08:00:45 +0700

    Auto commit on 2025-12-31 08:00:45

--- fitur/Tools/infoip.js
+++ fitur/Tools/infoip.js
@@ -11,34 +11,8 @@ async function message(sock, m, store) {
 
     if(!isset(arg)) return nyarios(`mana ipnya ?`);
     
-    const localhost = {
-        "status": "success",
-        "continent": "Asia",
-        "continentCode": "AS",
-        "country": "Indonesia",
-        "countryCode": "ID",
-        "region": "JB",
-        "regionName": "West Java",
-        "city": "Tasikmalaya",
-        "district": "",
-        "zip": "",
-        "lat": -7.3263,
-        "lon": 108.221,
-        "timezone": "Asia/Jakarta",
-        "offset": 25200,
-        "currency": "IDR",
-        "isp": "PT. Telekomunikasi Indonesia",
-        "org": "",
-        "as": "AS7713 PT Telekomunikasi Indonesia",
-        "asname": "telkomnet-as-ap",
-        "reverse": "",
-        "mobile": false,
-        "proxy": false,
-        "hosting": false,
-        "query": "36.74.41.116"
-    }
-
-    const ip = arg.includes("xiex.my.id") ? localhost : await sock.sendRequest(m).get(`http://ip-api.com/json/${arg}?fields=66846719`).then(v => v.data).catch(e => {error: "kesalahan input"});
+    const target = arg.toLocaleLowerCase().includes("localhost") ? "" : arg.startsWith("http") ? new URL(arg).hostname : arg
+    const ip = await sock.sendRequest(m).get(`http://ip-api.com/json/${target}?fields=66846719`).then(v => v.data).catch(e => { error: "kesalahan input" });
     const text = Object.keys(ip).map(v => `*${v}* = ${ip[v]}`).join("\n")
 
     nyarios(text);
