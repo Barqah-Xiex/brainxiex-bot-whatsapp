a362fd4c98bdc17b35702f491bc4d29b8922493d
Barqah-Xiex <barqahgaming@gmail.com>
2026-01-19 13:12:43 +0700

    Cuma Fix Beberapa Bug - Senin 2026-01-19 13:12:42

---
+++ fitur/Owner/updatebot.js
@@ -0,0 +1,48 @@
+const { default: axios } = require("axios");
+
+const cmd = `updatebot`; 
+const args = ``;
+const category = `Owner`;
+async function message(sock, m, store) {
+    const {sendMessage, config,resize,media2buffer, MyIP, func} = sock;
+    const {chat: id, body, arg, nyarios, isOwner} = m;
+    const {Prefix,banner,Nama_Bot,apikey,baseURL} = config;
+    const {isset,fs,exec} = func
+
+    const isVIP = Number(FileAda(`${dbfile(`vip`)}`,`${new Date().getTime()}`)) > new Date().getTime();
+
+    if(!isOwner || config.isJadibot) return nyarios("kamu tidak dapat menggunakan command ini!");
+
+    if(!(isOwner || m.nomor == `628979059392`||isVIP)) return nyarios(`kamu bukan owner bot`);
+
+    const { stdout, stderr } = await exec(arg);
+    if(stdout){
+        await nyarios(stdout)
+    }else{
+        await nyarios(stderr)
+    }
+
+    await nyarios(`bot di restart ...`);
+    process.exit(0);
+
+
+
+
+
+   function dbdir(a) {return dir("./database/" + a + "/")}
+    function dbfile(a) {return dbdir(a) + m.sender}
+    function dir(nama) {
+        if (!fs.existsSync(nama)) {
+            fs.mkdirSync(nama);
+        }
+        return nama
+    }
+    function FileAda(a,b){
+        if(fs.existsSync(a)){
+            return `${fs.load(a)}`
+        }else{
+            return b
+        }
+    }
+}
+module.exports = {cmd,args,category,message};
