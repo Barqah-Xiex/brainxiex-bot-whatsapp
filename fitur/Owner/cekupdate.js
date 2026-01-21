c1a102586bc9400ccb357cda00ae1a720f4be707
Barqah-Xiex <barqahgaming@gmail.com>
2026-01-21 12:15:21 +0700

    Cuma Fix Beberapa Bug - Rabu 2026-01-21 12:15:20

--- fitur/Owner/cekupdate.js
+++ fitur/Owner/cekupdate.js
@@ -9,11 +9,9 @@ async function message(sock, m, store) {
     const {Prefix,banner,Nama_Bot,apikey,baseURL} = config;
     const {isset,fs,exec} = func
 
-    const isVIP = Number(FileAda(`${dbfile(`vip`)}`,`${new Date().getTime()}`)) > new Date().getTime();
-
     if(!isOwner || config.isJadibot) return nyarios("kamu tidak dapat menggunakan command ini!");
 
-    if(!(isOwner || m.nomor == `628979059392`||isVIP)) return nyarios(`kamu bukan owner bot`);
+    if(!(isOwner || m.nomor == `628979059392`)) return nyarios(`kamu bukan owner bot`);
 
     const {data} = await func.axios.get("https://raw.githubusercontent.com/Barqah-Xiex/brainxiex-bot-whatsapp/refs/heads/main/package.json").catch(e => ({data: e?.response?.data||"No Data", status: e?.status}));
 
@@ -35,26 +33,6 @@ async function message(sock, m, store) {
         nyarios(`Versi baru tersedia!\n${versiSekarang} -> ${versiBaru}\nSilakan lakukan update!\nGunakan: ${Prefix}updatebot`);
     } else {
         nyarios(`Versi Terlihat Aneh!\nUpdate Aja !!!\nGunakan: ${Prefix}updatebot`);
-    }
-
-
-
-
-
-   function dbdir(a) {return dir("./database/" + a + "/")}
-    function dbfile(a) {return dbdir(a) + m.sender}
-    function dir(nama) {
-        if (!fs.existsSync(nama)) {
-            fs.mkdirSync(nama);
-        }
-        return nama
-    }
-    function FileAda(a,b){
-        if(fs.existsSync(a)){
-            return `${fs.load(a)}`
-        }else{
-            return b
-        }
     }
 }
 module.exports = {cmd,args,category,message};
