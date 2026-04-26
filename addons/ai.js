b763fa8349bcb967c0483205405bc92c74d4bc66
Barqah-Xiex <barqahgaming@gmail.com>
2026-04-26 22:23:25 +0700

    Cuma Fix Beberapa Bug - Minggu 2026-04-26 22:23:24

--- addons/ai.js
+++ addons/ai.js
@@ -46,17 +46,8 @@ module.exports = function (Barqah){
         let sum = await m.summarize();
         
         if(sum.isGroup) config.AI_Payload_Default.system += `\n\nData Group:${JSON.stringify({groupName:sum.groupName,groupDesc:sum.groupDesc})}`
-        delete sum.groupName;
-        delete sum.groupDesc;
-        messages.push({role:'user',content:`${prompt}\n\nData Chat:\n${JSON.stringify({
-            id: sum.id,
-            remoteJid: sum.remoteJid,
-            fromMe: sum.fromMe,
-            isGroup: sum.isGroup,
-            senderLid: sum.senderLid,
-            senderJid: sum.senderJid,
-            pushName: sum.pushName,
-        })}`});
+
+        messages.push({role:'user',content:`${prompt}`});
         try{
             const payload  = {
                 ...config.AI_Payload_Default,
